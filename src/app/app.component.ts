155c93ac9c6998b44abf182724624548253a969a
1mmense <turuntaev1997@gmail.com>
2017-11-23 23:12:36 +0400

    Whole new thing

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Post {
+     id: number;
+     name: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: '/app.component.html',
+})
+
+
+export class AppComponent{
+  mode = 'Observable';
+  errorMessage: string;
+  title = 'Listing posts';
+  public posts: any;
+  constructor(private http: HttpClient) {
+    this.http.get('https://aturuntaev-ruby-blog.herokuapp.com/posts.json')
+      .subscribe(
+        posts => this.posts = posts,
+        error => this.errorMessage = <any>error);
+  }  
+}
